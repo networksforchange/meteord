e76b977ea71d02725ea035aa7d28b9e0da9a814b
WayneUong <wayne@networksforchange.com>
2017-02-09 22:25:11 -0800

    add library

--- base/scripts/lib/install_wkhtmltopdf.sh
+++ base/scripts/lib/install_wkhtmltopdf.sh
@@ -5,7 +5,7 @@ WKHTMLTOPDF_TAR_FILE=wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
 cd /usr/local/share/
 curl -O http://download.gna.org/wkhtmltopdf/0.12/0.12.4/${WKHTMLTOPDF_TAR_FILE}
 tar -xf $WKHTMLTOPDF_TAR_FILE
-apt-get install libXrender1 -y
+apt-get install libXrender1 libfontconfig1 -y
 mv ./wkhtmltox/bin/wkhtmltopdf /bin
 
 rm $WKHTMLTOPDF_TAR_FILE
