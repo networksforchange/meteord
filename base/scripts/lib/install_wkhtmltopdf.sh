05aa27e0f834dfa62719ab1a01109c3daab1d2bc
Wayne Uong <wayne@networksforchange.com>
2017-10-16 19:51:10 -0700

    'fix link'

--- base/scripts/lib/install_wkhtmltopdf.sh
+++ base/scripts/lib/install_wkhtmltopdf.sh
@@ -3,7 +3,7 @@ set -e
 WKHTMLTOPDF_TAR_FILE=wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
 
 cd /usr/local/share/
-curl -O https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/${WKHTMLTOPDF_TAR_FILE}
+curl -O -L https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/${WKHTMLTOPDF_TAR_FILE}
 tar -xf $WKHTMLTOPDF_TAR_FILE
 apt-get install libXrender1 libfontconfig1 -y
 mv ./wkhtmltox/bin/wkhtmltopdf /bin
