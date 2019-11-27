5cb1714328dc8198a19cc5fb65cbdc496ffc38e2
Wayne Uong <wayne@networksforchange.com>
2019-11-26 19:30:22 -0800

    'update to debian 9'

--- base/scripts/lib/install_wkhtmltopdf.sh
+++ base/scripts/lib/install_wkhtmltopdf.sh
@@ -1,11 +1,8 @@
 # Install wkhtmltopdf
 set -e
-WKHTMLTOPDF_TAR_FILE=wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+WKHTMLTOPDF_DEB_FILE=wkhtmltox_0.12.5-1.stretch_amd64.deb
 
 cd /usr/local/share/
-curl -O -L https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/${WKHTMLTOPDF_TAR_FILE}
-tar -xf $WKHTMLTOPDF_TAR_FILE
-apt-get install libXrender1 libfontconfig1 -y
-mv ./wkhtmltox/bin/wkhtmltopdf /bin
-
-rm $WKHTMLTOPDF_TAR_FILE
+curl -O -L https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/${WKHTMLTOPDF_DEB_FILE}
+apt-get install -y ./${WKHTMLTOPDF_DEB_FILE}
+rm $WKHTMLTOPDF_DEB_FILE
